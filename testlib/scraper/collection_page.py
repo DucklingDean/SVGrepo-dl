aba11eef43de2f154214a43068f77e896be34fc9
DucklingDean <duckling.dean@proton.me>
2024-08-20 12:17:57 +0000

    [TEST] Update test for CollectionPage
    - Test CollectionPage.nextpage property.
    - Test multipule cases on each funtion.

--- testlib/scraper/collection_page.py
+++ testlib/scraper/collection_page.py
@@ -1,42 +1,57 @@
 from unittest           import TestCase
-from testlib.tools      import load_html , pp 
+from testlib.tools      import load_html , pp , test_cases
 from svgrepo_dl.scraper import CollectionPage
 
 
 
 
 class TestCollectionPage(TestCase):
-    def test_class(self):
-        for htmlfile in ['collection_page(1-page).html', 'collection_page(4-pages).html']:
-    
-            htmlcode = load_html(htmlfile)
-            clss = CollectionPage(htmlcode)
-            self.images_property(clss)
-            self.collection_property(clss)
-            self.nextpage_func_property(clss)
+    files = [
+        'collection_page(1-page).html',
+        'collection_page(4-pages).html',
+        'collection_page(21-page).html'
+    ]
+    cases = [CollectionPage(load_html(f)) for f in files]
+    del files
 
-    def images_property(self, clss:CollectionPage) -> None:
+
+
+
+    @test_cases
+    def test_images_property(self, clss:CollectionPage) -> None:
         imgs   = clss.images
         self.assertTrue(len(imgs)>0, "empty image list [].")
         pp(imgs)
 
-     
-    def collection_property(self, clss:CollectionPage) -> None:
+    @test_cases
+    def test_collection_property(self, clss:CollectionPage) -> None:
         title = clss.collection
         self.assertTrue(len(title)>0, "empty title.")
         pp(title)
 
-
-    def nextpage_func_property(self, clss:CollectionPage) -> None:
+    @test_cases
+    def test_nextpage_func_property(self, clss:CollectionPage) -> None:
         next_page_exists = clss.is_nextpage_exists()
         self.assertEqual(type(next_page_exists), bool)
-        pp({"next page": next_page_exists})
+        
 
         if next_page_exists:
             nextpage_url = clss.nextpage
             self.assertEqual(type(nextpage_url), str)
             self.assertEqual(clss.nextpage, clss.next_page)
-        pp({"next page url":clss.nextpage})
+
+        pp({
+            "next page url" :clss.nextpage,
+            "next page"     :next_page_exists
+        })
+
+
+    @test_cases
+    def test_pagecount_property(self, clss:CollectionPage) -> None:
+        pagecount = clss.pagecount
+        self.assertEqual(type(pagecount), int, "`CollectionPage.pagecount` should be an 'int'.")
+        self.assertGreater(pagecount, 0)
+        pp({'page count':pagecount})
 
 
 
