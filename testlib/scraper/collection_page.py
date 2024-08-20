3a328eeda9324f993b0a2730554ef48ca34ca4ec
DucklingDean <duckling.dean@proton.me>
2024-08-20 10:29:00 +0000

    [TEST] Update tests for CollectionPage
    Write every test in Its own function.

--- testlib/scraper/collection_page.py
+++ testlib/scraper/collection_page.py
@@ -6,29 +6,37 @@ from svgrepo_dl.scraper import CollectionPage
 
 
 class TestCollectionPage(TestCase):
-    def test_get_images(self):
+    def test_class(self):
         for htmlfile in ['collection_page(1-page).html', 'collection_page(4-pages).html']:
+    
             htmlcode = load_html(htmlfile)
-
             clss = CollectionPage(htmlcode)
+            self.images_property(clss)
+            self.collection_property(clss)
+            self.nextpage_func_property(clss)
 
-            imgs   = clss.images
-            self.assertTrue(len(imgs)>0, "empty image list [].")
-            pp(imgs)
+    def images_property(self, clss:CollectionPage) -> None:
+        imgs   = clss.images
+        self.assertTrue(len(imgs)>0, "empty image list [].")
+        pp(imgs)
 
-            title = clss.collection
-            self.assertTrue(len(title)>0, "empty title.")
-            pp(title)
+     
+    def collection_property(self, clss:CollectionPage) -> None:
+        title = clss.collection
+        self.assertTrue(len(title)>0, "empty title.")
+        pp(title)
 
-            next_page_exists = clss.is_nextpage_exists()
-            self.assertEqual(type(next_page_exists), bool)
-            pp({"next page": next_page_exists})
 
-            if next_page_exists:
-                nextpage_url = clss.nextpage
-                self.assertEqual(type(nextpage_url), str)
-                self.assertEqual(clss.nextpage, clss.next_page)
-            pp({"next page url":clss.nextpage})
+    def nextpage_func_property(self, clss:CollectionPage) -> None:
+        next_page_exists = clss.is_nextpage_exists()
+        self.assertEqual(type(next_page_exists), bool)
+        pp({"next page": next_page_exists})
+
+        if next_page_exists:
+            nextpage_url = clss.nextpage
+            self.assertEqual(type(nextpage_url), str)
+            self.assertEqual(clss.nextpage, clss.next_page)
+        pp({"next page url":clss.nextpage})
 
 
 
