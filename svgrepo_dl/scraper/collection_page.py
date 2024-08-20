d850645102697a107f3df73936e0c4fba146f174
DucklingDean <duckling.dean@proton.me>
2024-08-20 15:42:22 +0000

    [FEAT/REFACTOR] Add prev_page property & Refactor CollectionPage
    |prev_page: extract the URL of the Previous page if it exists else will return None
    |Refacting:
      - Butter code.
      - Using 'var_name' in vars name.
      - Add property `title`==`collection` .
      - Rename "*nextpage*" -> "*next_page*".
      - Remove "is_*page_exists"[F] functions.
      - Move property second name to the file tail.

--- svgrepo_dl/scraper/collection_page.py
+++ svgrepo_dl/scraper/collection_page.py
@@ -8,11 +8,16 @@ class CollectionPage(ScraperCore):
     """
     page with URL like: 'svgrepo.com/collection/**'
     """
-    _images    :list[dict]  = None
-    _collection:str         = None
-    _nextpage  :str|None    = None
-    _pagecount :int         = None
+    _images    : list[dict]  = None
+    _collection: str         = None
+    _next_page : str|None    = None
+    _prev_page : str|None    = None
+    _page_count: int         = None
     
+
+    _get_next_prev  = lambda self, index: self._tree.css(".style_pagingCarrier__NVbHL > div")[index].css_first("a")
+    _get_page_count = lambda self: int(self._tree.css_first(".style_pagingCarrier__NVbHL span").text().split("/")[-1].strip())
+
 
     @property 
     def images(self) -> list[dict]:
@@ -30,37 +35,45 @@ class CollectionPage(ScraperCore):
         return self._images
 
 
+
+
     @property
     def collection(self) -> str:
         """Collection Title"""
         if self._collection is None:
-            self._collection = self._get_collection_title()
+            self._collection = self._tree.css(".style_breadCrumbsScroller__93Cu8 div a")[-1].text().strip()
         return self._collection
+
+
 
     
     @property
     def next_page(self) -> str|None:
         """Next Page URL or None"""
-        return self.nextpage
-    
+        if self._next_page is None:
+            self._next_page = self._get_next_page_url()
+        return self._next_page
+
+
+
 
     @property
-    def nextpage(self) -> str|None:
-        """Next Page URL or None"""
-        if self._nextpage is None:
-            if self.is_nextpage_exists():
-                self._nextpage = self._get_nextpage_url()
-            else:
-                return None
-        return self._nextpage
-                           
+    def prev_page(self) -> str|None:
+        """Previous Page URL or None"""
+        if self._prev_page is None:
+            self._prev_page = self._get_prev_page_url()
+        return self._prev_page
+
+
+
 
     @property
-    def pagecount(self) -> int:
+    def page_count(self) -> int:
         """How many pages collection has"""
-        if self._pagecount is None:
-            self._pagecount = self._get_pagecount()
-        return self._pagecount
+        if self._page_count is None:
+            self._page_count = self._get_page_count()
+        return self._page_count
+
 
 
 
@@ -69,34 +82,52 @@ class CollectionPage(ScraperCore):
         elements = self._tree.css(".style_nodeListing__7Nmro img")
         return list(
             {
-            "title"     :img_element.attributes["alt"].strip(),
-            "url"       :img_element.attributes["src"].strip(),
-            "file-name" :img_element.attributes["src"].split("/")[-1].strip()
+                "title"     :img_element.attributes["alt"].strip(),
+                "url"       :img_element.attributes["src"].strip(),
+                "file-name" :img_element.attributes["src"].split("/")[-1].strip()
             } for img_element in elements
         )
 
 
-    def _get_collection_title(self) -> str:
-        "Scrape Collection Title."
-        return self._tree.css(".style_breadCrumbsScroller__93Cu8 div a")[-1].text().strip()
 
 
-    def is_nextpage_exists(self) -> bool:
-        return bool(
-            self._tree.css(".style_pagingCarrier__NVbHL div")[-1].css_first("a")
-        )
-
-    def _get_nextpage_url(self) -> str|None:
+    def _get_next_page_url(self) -> str|None:
         # i should not add this try catch, but some freaking nerds touch even "private fuctions"
         try:
-            return HTTPS_URL + self._tree.css(".style_pagingCarrier__NVbHL div")[-1].css_first("a").attributes['href'].strip()
-
+            return HTTPS_URL + self._get_next_prev(-1).attributes['href'].strip()
         except AttributeError:
             return None
 
 
-    def _get_pagecount(self) -> int:
-        return int(
-            self._tree.css_first(".style_pagingCarrier__NVbHL span").text()\
-            .split("/")[-1].strip() 
-        )
+
+
+    def _get_prev_page_url(self) -> str|None:
+        try:
+            return HTTPS_URL + self._get_next_prev(0).attributes['href'].strip()
+        except AttributeError:
+            return None
+
+
+
+
+
+
+
+
+
+
+# rename properties:
+
+    @property
+    def nextpage(self) -> str|None: return self.next_page
+
+    @property
+    def prevpage(self) -> str|None: return self.prev_page
+
+    @property
+    def pagecount(self) -> int: return self.page_count
+
+    @property
+    def title(self) -> str: return self.collection
+
+
