f3936fb9c98ec411a4b80e72586a165e243a5567
DucklingDean <duckling.dean@proton.me>
2024-09-01 11:35:34 +0000

    [FEAT] Implement of engine[M]
    - a module will contains all pre-existing tools & functions to make __main__ more clean.

---
+++ src/svgrepo_dl/engine/__init__.py
@@ -0,0 +1,3 @@
+from .tools import extract_all_collections 
+
+
---
+++ src/svgrepo_dl/engine/tools.py
@@ -0,0 +1,39 @@
+from reqio.pools import DefaultPool
+from reqio       import ReadyRequest
+from requests    import get as GET 
+from queue       import Queue
+from svgrepo_dl.scraper import HomePage
+from svgrepo_dl.scraper.collection_page import CollectionPage
+from svgrepo_dl.toolkit import print_progress
+
+__all__ = ["extract_all_collections",]
+
+def extract_all_collections() -> list[dict]:
+    headers = ReadyRequest.gen_headers()
+    headers["host"] = "www.svgrepo.com"
+
+    html = GET("https://www.svgrepo.com", headers=headers).content
+    total_pages:int = HomePage(html).total_pages
+    pool = DefaultPool(
+        pool_size=5,
+        save_files=False,
+        save_in_dict=True,
+    )
+    
+    tasks = Queue()
+    for page in range(1, total_pages+1):
+        tasks.put(pool.add(f"https://www.svgrepo.com/collections/all/{page}"))
+
+    print_progress(tasks, total_pages, "pg")
+    
+    collections = []
+    for html in pool.dict.values():
+        collections += HomePage(html).all_collections
+    return collections
+
+
+    
+
+
+
+
